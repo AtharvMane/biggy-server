031e9b0a34e296d933868d851c9ec09c39f12a91
AtharvMane <77918487+AtharvMane@users.noreply.github.com>
2021-08-24 23:45:20 +0530

    dotenv fixed

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+import dotenv from 'dotenv'
+dotenv.config()
 import express from 'express'
 import cors from "cors";
 import mongoose from "mongoose";
@@ -17,8 +19,7 @@ app.use('/Hotels',showPosts)
 app.use('/pendingOrders',addPendingOrder)
 app.use('/RestaurantSide',RestaurantSideOrders)
 app.use(errorHandler)
-
 const CONNECTION_URI=process.env.MONGO_CONNECTION_URI;
-mongoose.connect(CONNECTION_URI,{useNewUrlParser:true, useUnifiedTopology:true, useCreateIndex:true})
+mongoose.connect(CONNECTION_URI,{useNewUrlParser:true, useUnifiedTopology:true})
     .then(()=>{app.listen(PORT,()=>{console.log(`process running on port: ${PORT}`)})})
     .catch((error)=>{console.log(error.message)})
