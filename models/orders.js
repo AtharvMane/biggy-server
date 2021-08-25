b74abe56f72d74154d43102519042134b8bf1277
AtharvMane <77918487+AtharvMane@users.noreply.github.com>
2021-08-25 17:59:15 +0530

    added compulsory location parameter for orders

--- models/orders.js
+++ models/orders.js
@@ -1,7 +1,8 @@
 import mongoose from "mongoose";
 const orderSchema=new mongoose.Schema({
     cart: Array,
-    restaurant_id:String
+    restaurant_id:String,
+    location:{type:String, required:[true, "Please Provide Location" ]}
 })
 
 const Order=mongoose.model('order',orderSchema);
